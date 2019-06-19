a0e87aa748563eb6a0c9d42c9442b0c08a91ed56
Sandesh Bhagwat <sbhagwat@vmware.com>
2019-06-19 18:44:24 +0530

    Added Juniper SRX and condition to handle table joiner logic

--- network_insight_sdk_generic_datasources/juniper_srx/juniper_srx_pre_post_processor.py
+++ network_insight_sdk_generic_datasources/juniper_srx/juniper_srx_pre_post_processor.py
@@ -6,6 +6,7 @@ from network_insight_sdk_generic_datasources.parsers.text.pre_post_processor imp
 from network_insight_sdk_generic_datasources.parsers.common.block_parser import SimpleBlockParser
 from network_insight_sdk_generic_datasources.parsers.common.text_parser import GenericTextParser
 from network_insight_sdk_generic_datasources.parsers.common.block_parser import LineBasedBlockParser
+from network_insight_sdk_generic_datasources.parsers.common.line_parser import LineTokenizer
 
 
 class JuniperSRXDevicePrePostProcessor(PrePostProcessor):
@@ -57,8 +58,9 @@ class JuniperSwitchPortPrePostProcessor(PrePostProcessor):
                 if ip_address:
                     continue
                 output_line = "{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n".format(output_interface_name, administrative_status,
-                                                                  switch_port_mode, output_operational_status,
-                                                                  output_hardware_address, output_mtu, vlan, connected)
+                                                                        switch_port_mode, output_operational_status,
+                                                                        output_hardware_address, output_mtu, vlan,
+                                                                        connected)
                 output_lines.append(output_line)
         return '\n'.join(output_lines)
 
@@ -85,12 +87,12 @@ class JuniperRouterInterfacePrePostProcessor(PrePostProcessor):
 
     def pre_process(self, data):
         output_lines = []
-        skip_interface_names= [".local.", ".local..0", ".local..1",".local..2", "fab0.0", "fab1.0", "fxp1.0", "fxp2.0", "lo0.16384",
-                               "lo0.16385"]
+        skip_interface_names = [".local.", ".local..0", ".local..1", ".local..2", "fab0.0", "fab1.0", "fxp1.0",
+                                "fxp2.0", "lo0.16384", "lo0.16385"]
         parser = LineBasedBlockParser('Physical interface:')
         blocks = parser.parse(data)
         for block in blocks:
-            administrative_status="administrativeStatus: UP"
+            administrative_status = "administrativeStatus: UP"
             switch_port_mode = "switchPortMode: TRUNK"
             mtu = self.get_pattern_match(block, ".*Link-level type: .*, MTU: (.*?),")
             name = self.get_pattern_match(block, "Physical interface: (.*), Enabled.*")
@@ -106,16 +108,21 @@ class JuniperRouterInterfacePrePostProcessor(PrePostProcessor):
                 vlan = "vlan: {}".format(logical_name.split('.')[1]) if logical_name else "vlan"
                 output_interface_name = "name: {}".format(name)
                 output_operational_status = "operationalStatus: {}".format(ops_status)
-                output_hardware_address = "hardwareAddress: {}".format("" if hardware_address.isalpha() else hardware_address)
+                output_hardware_address = "hardwareAddress: {}".format("" if hardware_address.isalpha()
+                                                                       else hardware_address)
                 output_mtu = "mtu: {}".format(mtu if mtu.isdigit() else 0)
                 ip_address = self.get_pattern_match(block_1, ".*Local: (.*), Broadcast:.*")
                 if not ip_address or name in skip_interface_names:
                     continue
                 mask = self.get_pattern_match(block_1, ".*Destination: (.*), Local:.*")
                 output_ip_address = "ipAddress: {}/{}".format(ip_address, mask.split('/')[1])
-                output_line = "{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n".format(output_interface_name, administrative_status,
-                                                                  switch_port_mode, output_operational_status,
-                                                                  output_hardware_address, output_mtu, vlan, connected, output_ip_address)
+                output_line = "{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n".format(output_interface_name,
+                                                                            administrative_status,
+                                                                            switch_port_mode,
+                                                                            output_operational_status,
+                                                                            output_hardware_address,
+                                                                            output_mtu,
+                                                                            vlan, connected, output_ip_address)
                 output_lines.append(output_line)
         return '\n'.join(output_lines)
 
@@ -129,7 +136,8 @@ class JuniperRouterInterfacePrePostProcessor(PrePostProcessor):
             result.append(temp)
         return result
 
-    def get_pattern_match(self, line_block, pattern):
+    @staticmethod
+    def get_pattern_match(line_block, pattern):
         match_pattern = re.compile(pattern)
         for line in line_block.splitlines():
             match = match_pattern.match(line.strip())
@@ -144,7 +152,6 @@ class JuniperSRXRoutePrePostProcessor(PrePostProcessor):
         output_lines = []
         parser = LineBasedBlockParser('(.*): \d* destinations')
         blocks = parser.parse(data)
-        vrf = ""
         next_hop = "Next hop: (.*) via"
         next_hop_type = ".*Next hop type: (.*?), .*"
         interface = ".* via (.*),"
@@ -180,9 +187,10 @@ class JuniperSRXRoutePrePostProcessor(PrePostProcessor):
                         output_interface_name = "interfaceName: {}".format(output[0]['network_interface'])
                     else:
                         continue
-                    name = "name: {}".format(network_name + "_" + "{}".format(idx))
+                    name = "name: {}_{}".format(network_name, idx)
                     output_route_type = "routeType: {}".format(output[0]['route_type'])
-                    output_next_hop = "nextHop: {}".format(output[0]['next_hop'] if "next_hop" in output[0].keys() else "DIRECT")
+                    output_next_hop = "nextHop: {}".format(output[0]['next_hop'] if "next_hop" in output[0].keys()
+                                                           else "DIRECT")
                     output_network = "network: {}".format(network_name)
                     output_line = "{}\n{}\n{}\n{}\n{}\n{}\n".format(output_vrf, output_network, output_route_type,
                                                                     output_next_hop, output_interface_name, name)
@@ -199,7 +207,8 @@ class JuniperSRXRoutePrePostProcessor(PrePostProcessor):
             result.append(temp)
         return result
 
-    def get_pattern_match(self, line_block, pattern):
+    @staticmethod
+    def get_pattern_match(line_block, pattern):
         match_pattern = re.compile(pattern)
         match = match_pattern.match(line_block.strip())
         return match.groups()[0]
@@ -219,6 +228,7 @@ class JuniperMACTablePrePostProcessor(PrePostProcessor):
             result.append(temp)
         return result
 
+
 class JuniperSRXVRFPrePostProcessor(PrePostProcessor):
 
     def post_process(self, data):
@@ -226,7 +236,7 @@ class JuniperSRXVRFPrePostProcessor(PrePostProcessor):
         for d in data:
             temp = {}
             vrf_name = d.keys()[0]
-            router_id= [i["Router ID"] for i in d[vrf_name] if i.has_key("Router ID")]
+            router_id = [i["Router ID"] for i in d[vrf_name] if i.has_key("Router ID")]
             if "0.0.0.0" == router_id[0]: continue
             temp['name'] = "{}".format(vrf_name)
             result.append(temp)
@@ -235,11 +245,24 @@ class JuniperSRXVRFPrePostProcessor(PrePostProcessor):
 
 class JuniperNeighborsTablePrePostProcessor(PrePostProcessor):
 
+    def pre_process(self, data):
+        output_lines = []
+        for line in data.splitlines()[1:]:
+            line_tokenizer = LineTokenizer()
+            line_token = line_tokenizer.tokenize(line)
+            local_interface = "localInterface: {}".format(line_token[1])
+            remote_interface = "remoteInterface: {}".format(" ".join(line_token[3:-1]))
+            remote_device = "remoteDevice: {}".format(line_token[-1])
+            output_line = "{}\n{}\n{}\n".format(local_interface, remote_device, remote_interface)
+            output_lines.append(output_line)
+        return '\n'.join(output_lines)
+
     def post_process(self, data):
         result = []
         for d in data:
             temp = {}
-            for i in d:
-                temp[i] = d[i]
+            for i in d.split('\n'):
+                val = i.split(': ')
+                temp[val[0]] = val[1] if len(val) > 1 else ""
             result.append(temp)
         return result

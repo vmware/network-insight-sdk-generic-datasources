1342a90e1e9ebb768166661fb0d5ebf48b440290
Sandesh Bhagwat <sbhagwat@vmware.com>
2019-07-01 22:38:35 +0530

    Added testcase for juiper device version command

---
+++ test/routers_and_switches/juniper_srx/test_juniper_device_prepostprocess.py
@@ -0,0 +1,35 @@
+import unittest
+from network_insight_sdk_generic_datasources.routers_and_switches.juniper_srx.juniper_srx_pre_post_processor import JuniperDevicePrePostProcessor
+from network_insight_sdk_generic_datasources.parsers.common.vertical_table_parser import VerticalTableParser
+
+
+class JuniperDevicePrePostProcessorTestCase(unittest.TestCase):
+    def test_post_process(self):
+        parser = VerticalTableParser()
+        text = """\nnode0:\n--------------------------------------------------------------------------
+        \nHostname: fw1.rdm.nl.rgtn.com
+        \nModel: srx550m
+        \nJunos: 15.1X49-D150.2
+        \nJUNOS Software Release [15.1X49-D150.2]
+        \n\nnode1:
+        \n--------------------------------------------------------------------------
+        \nHostname: fw2.rdm.nl.rgtn.com
+        \nModel: srx550m
+        \nJunos: 15.1X49-D150.2
+        \nJUNOS Software Release [15.1X49-D150.2]\n'"""
+
+        expected_output = {'vendor': 'Juniper', 'name': 'Juniper-srx550m',
+                           'hostname': 'fw1.rdm.nl.rgtn.com',
+                           'ipAddress/fqdn': '10.40.13.37', 'model': 'srx550m', 'os': 'JUNOS 15.1X49-D150.2'}
+
+
+        data = parser.parse(text)
+        device = JuniperDevicePrePostProcessor()
+        result = device.post_process(data)
+        self.assertEqual(len(result), 1)
+        self.assertEqual(type(result), list)
+        self.assertEqual(expected_output, result[0])
+
+
+if __name__ == '__main__':
+    unittest.main()

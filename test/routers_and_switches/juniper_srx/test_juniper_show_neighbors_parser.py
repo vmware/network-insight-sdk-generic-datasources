56ef5f44a25298922a01737156700859fd4b5754
Sandesh Bhagwat <sbhagwat@vmware.com>
2019-07-02 09:16:14 +0530

    Added testcase for juiper device version command

--- test/routers_and_switches/juniper_srx/test_juniper_show_neighbors_parser.py
+++ test/routers_and_switches/juniper_srx/test_juniper_show_neighbors_parser.py
@@ -6,8 +6,8 @@ from network_insight_sdk_generic_datasources.parsers.common.horizontal_table_par
 class JuniperDevicePrePostProcessorTestCase(unittest.TestCase):
     def test_post_process(self):
         text = """\nLocal Interface    Parent Interface    Chassis Id          Port info          System Name
-ge-9/0/5           reth0               78:fe:3d:37:6e:c0   fw2.dp - ge-0/0/5 (ae3) sw1.rdm.nl.rgtn.com
-ge-0/0/5           reth0               78:fe:3d:37:6e:c0   fw1.dp - ge-0/0/5 (ae2) sw1.rdm.nl.rgtn.com"""
+            ge-9/0/5           reth0               78:fe:3d:37:6e:c0   fw2.dp - ge-0/0/5 (ae3) sw1.rdm.nl.rgtn.com
+            ge-0/0/5           reth0               78:fe:3d:37:6e:c0   fw1.dp - ge-0/0/5 (ae2) sw1.rdm.nl.rgtn.com"""
 
         expected_output = [{'localInterface': 'ge-9/0/5', 'remoteInterface': 'ge-0/0/5', 'remoteDevice': 'sw1.rdm.nl.rgtn.com'},
                            {'localInterface': 'ge-0/0/5', 'remoteInterface': 'ge-0/0/5', 'remoteDevice': 'sw1.rdm.nl.rgtn.com'}]

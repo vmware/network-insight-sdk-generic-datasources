8212526b71484827c6d26057efa404a2251eaae1
Sandesh Bhagwat <sbhagwat@vmware.com>
2019-08-01 18:53:40 +0530

    DELL switch: corrected logic to update interface name
    Signed-off-by: sandesh bhagwat <sbhagwat@vmware.com>

--- network_insight_sdk_generic_datasources/routers_and_switches/dell/dell_pre_post_processor.py
+++ network_insight_sdk_generic_datasources/routers_and_switches/dell/dell_pre_post_processor.py
@@ -197,15 +197,15 @@ class DellRoutesPrePostParser(PrePostProcessor):
                     interface_name = line[-1].lstrip()
                     routes = generic_parser.parse(line[0], self.route_rules)[0]
                     routes.update({"network": "{}".format(route_network['network'].rstrip())})
-                    routes.update({"name": "{}_{}".format(route_network['network'], idx)})
+                    routes.update({"name": "{}_{}".format(route_network['network'].rstrip(), idx)})
                     routes.update({"vrf": vrf})
-                    routes.update({"interfaceName": interface_name })
+                    routes.update({"interfaceName": interface_name})
                     # if 'loopback' in routes['interfaceName']:
                     #     continue
                     # if not routes['interfaceName']:
                     #     continue
                     routes.update({"routeType": "{}".format(self.route_types[route_type]
-                                                            if self.route_types.has_key(route_type) else "DIRECT")})
+                                                            if self.route_types.has_key(route_type) else "DYNAMIC")})
                     routes.update({"nextHop": "{}".format(routes['nextHop'] if route_type != 'C' else "DIRECT")})
                     result.append(routes.copy())
         except Exception as e:

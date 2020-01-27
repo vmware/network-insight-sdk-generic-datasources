6b6c771f2757a481358c9a4ceccd73ae19fef222
Piyush Roy <piyushr@vmware.com>
2020-01-27 16:44:35 +0530

    added XE logic

--- network_insight_sdk_generic_datasources/routers_and_switches/cisco/cisco_pre_post_processor.py
+++ network_insight_sdk_generic_datasources/routers_and_switches/cisco/cisco_pre_post_processor.py
@@ -6,30 +6,117 @@ import re
 from network_insight_sdk_generic_datasources.common.utilities import merge_dictionaries
 from network_insight_sdk_generic_datasources.parsers.text.pre_post_processor import PrePostProcessor
 from network_insight_sdk_generic_datasources.parsers.common.block_parser import SimpleBlockParser
+from network_insight_sdk_generic_datasources.parsers.common.block_parser import LineBasedBlockParser
+from network_insight_sdk_generic_datasources.parsers.text.text_processor import TextProcessor
+from network_insight_sdk_generic_datasources.parsers.text.text_processor import Rule
+from network_insight_sdk_generic_datasources.parsers.text.text_processor import rule_match_callback
 
 
-class CiscoASRDevicePrePostProcessor(PrePostProcessor):
+class CiscoASR1KXEDeviceInfoPrePostProcessor(PrePostProcessor):
 
-    def pre_process(self, data):
+    def parse(self, data):
         output_lines = []
+        d = dict()
         lines = data.splitlines()
         for line in lines:
-            print("parsing line {}".format(line))
             if 'board' in line:
-                output_lines.append('serial: ' + line.split(' ')[-1])
+                d['serial'] = line.split(' ')[-1]
             if 'uptime' in line:
-                output_lines.append('name: {}'.format(line.split(' ')[0]))
-                output_lines.append('hostname: {}'.format(line.split(' ')[0]))
-            if 'ASR' in line:
-                output_lines.append('model: ' + line.split(' ')[1])
-        output_lines.append('os: NXOS')
-        output_lines.append('vendor: Cisco')
-        output_lines.append('haState: ACTIVE')
+                d['name'] = line.split(' ')[0]
+                d['hostname'] = line.split(' ')[0]
+            if 'IOS Software' in line:
+                d['model'] = '{} {}'.format(line.split(',')[1].strip(), line.split(',')[2].strip())
+        d['os'] = 'IOS'
+        d['vendor'] = 'Cisco'
+        d['haState'] = 'ACTIVE'
+        output_lines.append(d)
+        return output_lines
 
-        return '\n'.join(output_lines)
 
-    def post_process(self, data):
-        return [merge_dictionaries(data)]
+class CiscoASRXERoutePrePostProcessor(PrePostProcessor):
+    def parse(self, data):
+        output_lines = []
+        if 'Gateway of last resort' not in data:
+            return output_lines
+
+        vrf = 'default'
+        lines = data.splitlines()
+        for line in lines:
+            d = dict()
+            fields = re.split('\\s+', line)
+            if 'via' in line:
+                d['name'] = fields[1]
+                d['network'] = fields[1]
+                d['nextHop'] = fields[4].rstrip(',')
+                d['routeType'] = fields[0]
+                d['interfaceName'] = fields[-1]
+            if 'Routing Table' in line:
+                vrf = fields[-1]
+            if 'directly connected' in line:
+                d['name'] = fields[1]
+                d['network'] = fields[1]
+                d['nextHop'] = 'DIRECT'
+                d['routeType'] = 'DIRECT'
+                d['interfaceName'] = fields[-1]
+            if bool(d):
+                output_lines.append(d)
+        for i in output_lines:
+            i.update(vrf=vrf)
+        return output_lines
+
+
+class CiscoASRXEVRFPrePostProcessor(PrePostProcessor):
+    def parse(self, data):
+        output_lines = [dict(vrf='default')]
+        lines = data.splitlines()
+        for line in lines[1:]:
+            if line[:30].strip().split(' ')[0] != '':
+                d = dict()
+                d['vrf'] = line[:30].strip().split(' ')[0]
+                output_lines.append(d)
+        return output_lines
+
+
+class CiscoASRXEInterfacesPrePostProcessor(PrePostProcessor):
+    def parse(self, data):
+
+        name_regex = "(.*) is (administratively )?(up|down), .*"
+        mtu_regex = "MTU (\\d+) bytes.*"
+        ip_regex = "Internet address is (.*)"
+        interface_speed_regex = ".* BW (\\d+) .*"
+        operational_speed_regex = interface_speed_regex
+        administrative_status_regex = ".* is (?:administratively )?(up|down), .*"
+        operational_status_regex = ".* line protocol is (up|down)"
+        hardware_address_regex = ".* address is (\\w+\\.\\w+\\.\\w+) .*"
+        duplex_regex = "(.*) Duplex.*"
+        # connected_regex = ""
+        # switch_port_mode_regex = ""
+
+        parser = TextProcessor(LineBasedBlockParser('line protocol'))
+        parser.add_rule(Rule('name', name_regex, rule_match_callback))
+        parser.add_rule(Rule('mtu', mtu_regex, rule_match_callback))
+        parser.add_rule(Rule('ipAddress', ip_regex, rule_match_callback))
+        parser.add_rule(Rule('interfaceSpeed', interface_speed_regex, rule_match_callback))
+        parser.add_rule(Rule('operationalSpeed', operational_speed_regex, rule_match_callback))
+        parser.add_rule(Rule('administrativeStatus', administrative_status_regex, rule_match_callback))
+        parser.add_rule(Rule('operationalStatus', operational_status_regex, rule_match_callback))
+        parser.add_rule(Rule('hardwareAddress', hardware_address_regex, rule_match_callback))
+        parser.add_rule(Rule('duplex', duplex_regex, rule_match_callback))
+        output_lines = parser.process(data)
+        if not bool(output_lines):
+            return []
+
+        for r in output_lines:
+            r.update(duplex=r['duplex'].upper())
+            r.update(interfaceSpeed=int(r['interfaceSpeed']) * 1000)
+            r.update(operationalSpeed=int(r['interfaceSpeed']) * 1000)
+            r.update(administrativeStatus=r["administrativeStatus"].upper())
+            r.update(operationalStatus=r["operationalStatus"].upper())
+            r.update(connected="TRUE" if r['administrativeStatus'] == 'UP' else "FALSE")
+            r.update(switchPortMode="ACCESS")
+            print(r)
+
+        return output_lines
 
 
 class CiscoDevicePrePostProcessor(PrePostProcessor):

e5df4f01dd801d6a35aa4d5214a5527a521d3b97
Piyush Roy <piyushr@vmware.com>
2020-04-16 01:23:42 +0530

    Revert "Handle Routes with NextHopVRF (#47)"
    This reverts commit 12ccb3b609b7348a5c5d4b9fb0437c31279111d6.

--- network_insight_sdk_generic_datasources/common/constants.py
+++ network_insight_sdk_generic_datasources/common/constants.py
@@ -33,7 +33,6 @@ JOINED_TABLE_ID_KEY = 'joined_table_id'
 RESULT_WRITER_KEY = 'result_writer'
 PATH_KEY = 'path'
 GENERATION_DIRECTORY_KEY = 'generation_directory'
-OFFLINE_COMMAND_RESULTS_DIRECTORY_KEY = 'offline_command_results_directory'
 # ############ End of YAML Keys ################## #
 
 

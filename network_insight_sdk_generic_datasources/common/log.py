3e935ece9294ae22baa48e2f7901cd10ea7356c1
Trey Tyler <ttyler@vmware.com>
2021-12-07 07:41:04 -0700

    minor change

--- network_insight_sdk_generic_datasources/common/log.py
+++ network_insight_sdk_generic_datasources/common/log.py
@@ -42,7 +42,6 @@ def configure_logger(file_name=None):
     # add the handler to the root logger
     logging.getLogger().addHandler(console)
     py_logger = logging.getLogger()
-    netmiko = logging.getLogger("netmiko")
 
     py_logger.critical("Log file Name: %s", tmp_file)
 
